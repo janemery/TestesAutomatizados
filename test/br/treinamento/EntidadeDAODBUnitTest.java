7c3ee3a257aef3219942449fcf71f8f2da03be8e
Jane Mery <janymeryc@gmail.com>
2014-02-12 23:39:54 -0300

    Incluindo configuração e arquivos para dbunit.

---
+++ test/br/treinamento/EntidadeDAODBUnitTest.java
@@ -0,0 +1,29 @@
+package br.treinamento;
+
+import java.io.FileInputStream;
+
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+
+public class EntidadeDAODBUnitTest extends DatabaseTestCase {
+	//private EntidadeDAODBUnitTest persistencia;
+	
+	//private final static String TABELA_ENTIDADE = "entidade";
+	//@Test
+
+	
+	
+	@Override
+	protected IDatabaseConnection getConnection() throws Exception {
+		return new DatabaseConnection(ConnectionFactory.getConnection());
+	}
+
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		return new FlatXmlDataSetBuilder().build(new FileInputStream("NewFile.xml"));
+	}
+}
